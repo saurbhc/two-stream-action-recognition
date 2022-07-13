7aaca584e2640ec5df8a4296f3d73f9a45a9df75
Saurabh Chopra <Saurabh.Chopra.2021@live.rhul.ac.uk>
2022-07-13 20:53:06 +0000

    update setup_dataset script -- quiet unzip

--- scripts/setup_dataset.sh
+++ scripts/setup_dataset.sh
@@ -12,7 +12,7 @@ wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_jpegs_256.zip.0
 wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_jpegs_256.zip.002
 wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_jpegs_256.zip.003
 cat ucf101_jpegs_256.zip* > ucf101_jpegs_256.zip
-unzip ucf101_jpegs_256.zip | pv -l >/dev/null \
+unzip -q ucf101_jpegs_256.zip \
     && rm ucf101_jpegs_256.zip \
     && rm ucf101_jpegs_256.zip.001 \
     && rm ucf101_jpegs_256.zip.002 \
@@ -23,7 +23,7 @@ wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_tvl1_flow.zip.0
 wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_tvl1_flow.zip.002
 wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_tvl1_flow.zip.003
 cat ucf101_tvl1_flow.zip* > ucf101_tvl1_flow.zip
-unzip ucf101_tvl1_flow.zip | pv -l >/dev/null \
+unzip -q ucf101_tvl1_flow.zip \
     && rm ucf101_tvl1_flow.zip \
     && rm ucf101_tvl1_flow.zip.001 \
     && rm ucf101_tvl1_flow.zip.002 \
